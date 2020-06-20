b52b650b1b0935fb686a92403682c22f987ab95c
David <davidlago104@gmail.com>
2020-06-20 15:26:34 -0400

    added function to front-end

--- front-end/index.js
+++ front-end/index.js
@@ -1,5 +1,8 @@
-// const sneakerButton = document.getElementById("show-sneakers")
+const sneakerButton = document.getElementById("show-sneakers")
 
-// sneakerButton.addEventListener(sneaker, e){
-
-// }
+sneakerButton.addEventListener("click", e){
+    fetch('http://localhost:3000/sneakers')
+    .then(function(res){
+        return res.json()
+    })
+}
